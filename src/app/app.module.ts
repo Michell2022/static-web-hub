830160ae61b6659dcb6b8dde6971eebf82d01e1d
michell <116586325+Michell2022@users.noreply.github.com>
2023-10-03 00:34:40 -0500

    pages y components creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TrainningComponent } from './pages/trainning/trainning.component';
+import { CommunityComponent } from './pages/community/community.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    AboutComponent,
+    TrainningComponent,
+    CommunityComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
