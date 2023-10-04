abd5ddf30192db97060243b3aea91793866f4ce8
michell <116586325+Michell2022@users.noreply.github.com>
2023-10-03 19:59:06 -0500

    Routes creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { TrainningComponent } from './pages/trainning/trainning.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CommunityComponent } from './pages/community/community.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'trainning', component: TrainningComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'community', component: CommunityComponent },
+  { path: 'contact', component: ContactComponent},
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
